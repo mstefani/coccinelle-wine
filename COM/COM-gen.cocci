mstefani/coccinelle-wine

commit fb4ec9a21eba76394934171e3d6b3c046898f8cb

COM: Change the fallback for the lpVtbl to IIFace wrapper replacement.

COM/COM-gen.cocci
@@ -352,20 +352,19 @@ identifier iface, wrapper;
 )
 
 @@
-identifier wrapcast.wrapper;
-type T;
-T *obj;
-@@
-- wrapper(obj)
-+ &obj->%s
-
-@@
-expression obj;
+%s obj;
 identifier wrapcast.wrapper;
 @@
 - wrapper(obj)
 + obj.%s
-""" % (IIFace, lpVtbl, lpVtbl, IIFace_iface, IIFace_iface))
+
+@@
+identifier wrapcast.wrapper;
+expression obj;
+@@
+- wrapper(obj)
++ &obj->%s
+""" % (IIFace, lpVtbl, lpVtbl, Object, IIFace_iface, IIFace_iface))
 
 print("""
 // Sanity: impl_from%s() should be used only from %s members

commit 2309e87dcfc619774802fb7c09bae50d7a987f7d

COM: Fix the lpVtbl to IIFace wrapper replacement for good.
This also fixes a regression introduced in fb4ec9a21eba.

COM/COM-gen.cocci
@@ -370,19 +370,17 @@ identifier iface, wrapper;
 )
 
 @@
-%s obj;
-identifier wrapcast.wrapper;
-@@
-- wrapper(obj)
-+ obj.%s
-
-@@
 identifier wrapcast.wrapper;
 expression obj;
 @@
+(
+- wrapper(&obj)
++ &obj.%s
+|
 - wrapper(obj)
 + &obj->%s
-""" % (IIFace, lpVtbl, lpVtbl, Object, IIFace_iface, IIFace_iface))
+)
+""" % (IIFace, lpVtbl, lpVtbl, IIFace_iface, IIFace_iface))
 
 print("""
 // Sanity: impl_from%s() should be used only from %s members