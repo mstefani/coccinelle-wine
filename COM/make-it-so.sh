db69c65f527050f841f115d909e649fe58e530d3
Michael Stefaniuc <mstefani@redhat.com>
2011-04-20 20:29:58 +0200

    COM: Use by default an in source tree Wine build.

--- COM/make-it-so.sh
+++ COM/make-it-so.sh
@@ -4,12 +4,13 @@
 #       Wrapper around the COM cocci genrator cocci file.
 
 
-WINE="$HOME/work/wine"
+#WINE="$HOME/work/wine"
+WINE="/j/wine/source/COM"
 MYDIR=`dirname $0`
 DATE=`date +%Y%m%d-%H%M`
 GENCOCCI=`mktemp --tmpdir COM-$DATE-XXXX.cocci`
 
-spatch -sp_file "$MYDIR/COM-gen.cocci" $@ > "$GENCOCCI" || exit 1
+spatch -sp_file "$MYDIR/COM-gen.cocci" -I "$WINE/include" $@ > "$GENCOCCI" || exit 1
 if [ -s "$GENCOCCI" ]; then
     echo Generated $GENCOCCI >&2
     spatch -sp_file "$GENCOCCI" -macro_file "$MYDIR/../macros" -patch "$WINE" -smpl_spacing $@
